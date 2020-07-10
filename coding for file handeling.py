053674c7babb3921130d9bead32cfd170ee54fde
iramirshad <55203200+iramirshad@users.noreply.github.com>
2020-07-10 12:32:12 +0500

    2nd commit of same file

--- coding for file handeling.py
+++ coding for file handeling.py
@@ -1,10 +1,18 @@
-from itertools import dropwhile, islice
-str1 = []
-str2 = []
-file1 = open("txt1.txt", "r")
-print(file1.readlines()[1])
-for fnam in file1:
-    with open(fnam) in file1:
-        start_at = dropwhile(lambda L: '.' not in L.splite(), file1)
-        for line in islice(start_at, 1, None):
-           print(line)
+def greedy_activity_selector():
+
+    a = []
+    k = 0
+    file1 = open('txt1.txt', 'r')
+    s = file1.readline()
+
+    n = len(s)
+    for m in range(0, n):
+        if s[m] != '.':
+            a.append(s[m])
+        else:
+            break
+        print(a)
+
+
+
+print(greedy_activity_selector())
